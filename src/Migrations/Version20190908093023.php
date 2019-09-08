4309142ed61f581bd31ebbf1c8620ca709c11ccf
outbid <outbid1990@gmail.com>
2019-09-08 13:00:27 +0300

    migrations

---
+++ src/Migrations/Version20190908093023.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190908093023 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE expense (id INT AUTO_INCREMENT NOT NULL, item_id INT DEFAULT NULL, count INT NOT NULL, amount NUMERIC(10, 0) NOT NULL, date DATE NOT NULL, time TIME NOT NULL, INDEX IDX_2D3A8DA6126F525E (item_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE expense ADD CONSTRAINT FK_2D3A8DA6126F525E FOREIGN KEY (item_id) REFERENCES item (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE expense DROP FOREIGN KEY FK_2D3A8DA6126F525E');
+        $this->addSql('DROP TABLE expense');
+        $this->addSql('DROP TABLE item');
+    }
+}
